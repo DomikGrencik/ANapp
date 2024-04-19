bab77fe8baeaebb27c162995f304f3a17af16bef
Dominik Grencik <dominikgrencik@gmail.com>
2024-04-19 22:42:24 +0200

    modal v1.0

--- AN-FRONTEND/AutoNetworkApp/src/ts/components/MyModal.tsx
+++ AN-FRONTEND/AutoNetworkApp/src/ts/components/MyModal.tsx
@@ -1,14 +1,5 @@
 import { FC, ReactNode } from 'react';
-import {
-  CircularProgress,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material';
+import { CircularProgress } from '@mui/material';
 
 import useFetchInterfaces from '../utils/hooks/useFetchInterfaces';
 
@@ -29,48 +20,8 @@ const Modal: FC<ModalProps> = ({
   hasTable,
   idDevice,
 }) => {
-  /* const fetchInterfacesOfDevice = async () => {
-    const response = await fetch(
-      `${API_ROUTE_BASE}interface_of_devices/getInterfacesOfDevice/${idDevice}`,
-      {
-        method: 'GET',
-      }
-    );
-    const json = await response.json();
-
-    return dataSchemaInterface.parse(json);
-  };
-
-  const {
-    isLoading: isLoadingInterfaces,
-    error: errorInterfaces,
-    data: dataInterfaces,
-  } = useQuery({
-    queryKey: ['interfaces', idDevice],
-    queryFn: fetchInterfacesOfDevice,
-  });
-
-  if (errorInterfaces) {
-    console.error(errorInterfaces.message);
-    return null;
-  } */
-  /*  const [filteredData, setFilteredData] = useState({
-    id: 0,
-    name: '',
-    type: '',
-    interface_id: 0,
-    IP_address: '',
-    connector: '',
-    AN: '',
-    speed: '',
-    interface_id2: 0,
-  }); */
-
-  const {
-    data: dataInterfaces,
-    isLoading: isLoadingInterfaces,
-    error,
-  } = useFetchInterfaces();
+  const { data, isLoading, error } = useFetchInterfaces();
+  const filteredData = data?.filter((item) => item.id === idDevice);
 
   if (error) {
     console.error(error.message);
@@ -80,10 +31,6 @@ const Modal: FC<ModalProps> = ({
   if (!isOpen) {
     return null;
   }
-
-  //setFilteredData(dataInterfaces?.filter((item) => item.id === idDevice));
-
-  //const filteredData = dataInterfaces?.filter((item) => item.id === idDevice);
 
   return (
     <div
@@ -103,56 +50,35 @@ const Modal: FC<ModalProps> = ({
         <div className="my-modal__content">
           {children}
           {hasTable ? (
-            <>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 250 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>interface_id</TableCell>
-                      <TableCell align="right">name</TableCell>
-                      <TableCell align="right">IP address</TableCell>
-                      <TableCell align="right">interface_id2</TableCell>
-                      <TableCell align="right">id</TableCell>
-                      <TableCell align="right">type</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {dataInterfaces?.map(
-                      ({
-                        interface_id,
-                        name,
-                        IP_address,
-                        interface_id2,
-                        id,
-                        type,
-                      }) => (
-                        <TableRow
-                          // eslint-disable-next-line react/no-array-index-key
-                          key={interface_id}
-                          sx={{
-                            '&:last-child td, &:last-child th': { border: 0 },
-                          }}
-                        >
-                          <TableCell component="th" scope="row">
-                            {interface_id}
-                          </TableCell>
-                          <TableCell align="right">{name}</TableCell>
-                          <TableCell align="right">{IP_address}</TableCell>
-                          <TableCell align="right">{interface_id2}</TableCell>
-                          <TableCell align="right">{id}</TableCell>
-                          <TableCell align="right">{type}</TableCell>
-                        </TableRow>
-                      )
-                    )}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              {isLoadingInterfaces ? (
-                <div>
-                  <CircularProgress sx={{ margin: '20px' }} />
+            <div className="my-table">
+              <div className="my-table__layout my-table__layout-header">
+                <div>int_id</div>
+                <div>name</div>
+                <div>id</div>
+                <div>type</div>
+              </div>
+              {isLoading ? (
+                <div className="my-table__loading-wrapper">
+                  <div className="my-table__loading">
+                    <CircularProgress sx={{ color: '#d6d9dd' }} />
+                  </div>
                 </div>
-              ) : null}
-            </>
+              ) : (
+                <div className="my-table__body">
+                  {filteredData?.map(({ interface_id, name, id, type }) => (
+                    <div
+                      className="my-table__layout my-table__layout-body"
+                      key={interface_id}
+                    >
+                      <div>{interface_id}</div>
+                      <div>{name}</div>
+                      <div>{id}</div>
+                      <div>{type}</div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           ) : null}
         </div>
         <div>
--- AN-FRONTEND/AutoNetworkApp/src/ts/components/MyTable.tsx
+++ AN-FRONTEND/AutoNetworkApp/src/ts/components/MyTable.tsx
@@ -2,12 +2,11 @@ import { FC, useState } from 'react';
 import { CircularProgress } from '@mui/material';
 import { z } from 'zod';
 
-import { dataSchemaDevices } from '../pages/Database';
+import { dataSchemaDevices } from '../types/data-types';
 
 import MyModal from './MyModal';
 
 interface TableProps {
-  //onClick: () => void;
   data: z.infer<typeof dataSchemaDevices>;
   isLoading?: boolean;
 }
@@ -24,46 +23,45 @@ const MyTable: FC<TableProps> = ({ data, isLoading }) => {
 
   return (
     <>
-        <div className="my-table">
-          <div className="my-table__layout my-table__layout-header">
-            <div>ID</div>
-            <div>name</div>
-            <div>type</div>
-            <div>device_id</div>
-          </div>
-          {isLoading ? (
-            <div className="my-table__loading-wrapper">
-              <div className="my-table__loading">
-                <CircularProgress sx={{ color: '#d6d9dd' }} />
-              </div>
-            </div>
-          ) : (
-            <div className="my-table__body">
-              {data?.map(({ id, name, type, device_id }) => (
-                <div
-                  className="my-table__layout my-table__layout-body"
-                  onClick={() => {
-                    setOpen(true);
-                    setDevData({ id, name, type, device_id });
-                  }}
-                  onKeyDown={(e) => {
-                    e.key === 'Enter' &&
-                      (setOpen(true),
-                      setDevData({ id, name, type, device_id }));
-                  }}
-                  key={id}
-                  role="button"
-                  tabIndex={0}
-                >
-                  <div>{id}</div>
-                  <div>{name}</div>
-                  <div>{type}</div>
-                  <div>{device_id}</div>
-                </div>
-              ))}
-            </div>
-          )}
+      <div className="my-table">
+        <div className="my-table__layout my-table__layout-header">
+          <div>ID</div>
+          <div>name</div>
+          <div>type</div>
+          <div>device_id</div>
         </div>
+        {isLoading ? (
+          <div className="my-table__loading-wrapper">
+            <div className="my-table__loading">
+              <CircularProgress sx={{ color: '#d6d9dd' }} />
+            </div>
+          </div>
+        ) : (
+          <div className="my-table__body">
+            {data?.map(({ id, name, type, device_id }) => (
+              <div
+                className="my-table__layout my-table__layout-body my-table__layout-body-interactive"
+                onClick={() => {
+                  setOpen(true);
+                  setDevData({ id, name, type, device_id });
+                }}
+                onKeyDown={(e) => {
+                  e.key === 'Enter' &&
+                    (setOpen(true), setDevData({ id, name, type, device_id }));
+                }}
+                key={id}
+                role="button"
+                tabIndex={0}
+              >
+                <div>{id}</div>
+                <div>{name}</div>
+                <div>{type}</div>
+                <div>{device_id}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
       <MyModal
         isOpen={open}
