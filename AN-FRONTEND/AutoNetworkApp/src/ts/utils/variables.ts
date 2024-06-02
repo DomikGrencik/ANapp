435f9e90060837d6b0021f819dada2fb72965bd4
Dominik Grencik <dominikgrencik@gmail.com>
2024-06-02 18:31:56 +0200

    dockerized

--- AN-FRONTEND/AutoNetworkApp/src/ts/utils/variables.ts
+++ AN-FRONTEND/AutoNetworkApp/src/ts/utils/variables.ts
@@ -1,3 +1,3 @@
-export const API_ROUTE_BASE = 'http://127.0.0.1:80/api/';
+export const API_ROUTE_BASE = 'http://147.229.192.56:80/api/';
 
 export const FONT_SIZE_DEFAULT = 16;
--- AN-FRONTEND/AutoNetworkApp/vite.config.ts
+++ AN-FRONTEND/AutoNetworkApp/vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    watch: {
-      usePolling: true,
-    },
+    host: true,
+    strictPort: true,
+    port: 80,
   },
   plugins: [react()],
 });
